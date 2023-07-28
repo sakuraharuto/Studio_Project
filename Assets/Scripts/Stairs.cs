6a46f24e809937a540d06a4a081dcf8b5de514f0
Trigger0v0 <t2309190556@gmail.com>
2023-07-28 10:27:06 -0400

    update

---
+++ Assets/Scripts/Stairs.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stairs : MonoBehaviour
+{
+    public Transform teleportTarget;
+    public GameObject thePlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
